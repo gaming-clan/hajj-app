0d588e778ddc1ad9b668bf2ecf24a08a043fefb2
Dario Lloshi <dariolloshi2023@gmail.com>
2025-06-13 12:20:38 +0200

    Refactor package name from com.example.hajjrules to com.muslim.hajjrules

---
+++ hajj_app/app/src/main/java/com/muslim/hajjrules/model/HajjRule.java
@@ -0,0 +1,31 @@
+package com.muslim.hajjrules.model;
+
+public class HajjRule {
+    private String title;
+    private String description;
+    private int categoryId;
+    private int id;
+
+    public HajjRule(String title, String description, int categoryId, int id) {
+        this.title = title;
+        this.description = description;
+        this.categoryId = categoryId;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
